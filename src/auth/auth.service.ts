6655dba21fbc6a3d17bfbc33b4baaabdd40599c0
holfizz <gorlach7v@gmial.com>
2024-03-16 20:40:03 +0300

    update:changed token life from 1 hour to 1 day

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
       const data = { id: userId };
 
       const accessToken = this.jwt.sign(data, {
-        expiresIn: "1h",
+        expiresIn: "1d",
       });
 
       const refreshToken = this.jwt.sign(data, {
