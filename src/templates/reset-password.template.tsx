0ebb8e118c747a72e5d35128e9e46ba3126a5a31
holfizz <gorlach7v@gmial.com>
2024-08-24 19:50:10 +0300

    style: add new email template

--- src/templates/reset-password.template.tsx
+++ src/templates/reset-password.template.tsx
@@ -1,6 +1,6 @@
 import { Body, Container, Font, Head, Heading, Html, Link, Text } from "@react-email/components";
 
-export function ConfirmRegistrationTemplate({ link, email, name }: { link: string; email: string; name: string }) {
+export function ResetPasswordTemplate({ link, email }: { link: string; email: string }) {
   return (
     <Html lang="ru">
       <Head>
@@ -18,30 +18,31 @@ export function ConfirmRegistrationTemplate({ link, email, name }: { link: strin
       <Body style={{ backgroundColor: "#f8f4f0", padding: "20px", fontFamily: "Roboto, Verdana, sans-serif" }}>
         <Container style={{ maxWidth: "600px", margin: "0 auto", backgroundColor: "#ffffff", padding: "20px", borderRadius: "20px", boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)" }}>
           <Heading as="h1" style={{ color: "#ffb57f", fontSize: "24px", lineHeight: "32px", textAlign: "center", marginBottom: "20px" }}>
-            Привет, {name}!
+            Сброс пароля
           </Heading>
           <Text style={{ color: "#272727", fontSize: "16px", lineHeight: "24px", textAlign: "center", marginBottom: "20px" }}>
-            Вы зарегистрировались на платформе <strong>whai.ru</strong> с почтовым адресом {email}. Для активации вашего аккаунта, пожалуйста, нажмите на ссылку ниже:
+            Вы запросили сброс пароля для адреса {email}. Пожалуйста, нажмите на ссылку ниже, чтобы ввести новый пароль:
           </Text>
-          <Link
-            href={link}
-            style={{
-              display: "inline-block",
-              padding: "12px 20px",
-              fontSize: "16px",
-              lineHeight: "24px",
-              color: "#ffffff",
-              backgroundColor: "#ffb57f",
-              borderRadius: "16px",
-              textDecoration: "none",
-              textAlign: "center",
-            }}
-          >
-            Активировать аккаунт
-          </Link>
+          <Container align="center">
+            <Link
+              href={link}
+              style={{
+                display: "inline-block",
+                padding: "16px 24px",
+                fontSize: "16px",
+                lineHeight: "24px",
+                color: "#ffffff",
+                backgroundColor: "#ffb57f",
+                borderRadius: "30px",
+                textDecoration: "none",
+                textAlign: "center",
+              }}
+            >
+              Сбросить пароль
+            </Link>
+          </Container>
           <Text style={{ color: "#828282", fontSize: "14px", lineHeight: "20px", textAlign: "center", marginTop: "20px" }}>
-            Если вы не регистрировались на нашем сервисе, пожалуйста, проигнорируйте это письмо и не переходите по ссылке. Если у вас возникли вопросы, свяжитесь с нашей поддержкой
-            по почте{" "}
+            Если вы не запрашивали сброс пароля, проигнорируйте это письмо. Если у вас возникли вопросы, свяжитесь с нашей поддержкой по почте{" "}
             <a href="mailto:support@whai.ru" style={{ color: "#ffb57f" }}>
               support@whai.ru
             </a>
